95ac4b3b78cb7b24ccb3c5d6509e4a609137761c
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-20 03:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = "someone";
 
-const welcomeMessage = "Welcome to Flatbook, "+ currentUser 
+const welcomeMessage = "Welcome to Flatbook, "+ currentUser "!"
